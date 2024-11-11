96d03c4c8edd9d1c4e3f7c540f628d86c5051cd0
danco <daniel.coombs@mohawkcollege.ca>
2024-11-11 13:47:38 -0500

    add test collision

--- GAME 10003 Game Development Foundations - 2D Game Template (v1.2)1/Game.cs
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.2)1/Game.cs
@@ -11,6 +11,17 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
+        Vector2 rectangleLocation = new Vector2(200, 400);
+        Vector2 rectangleSize = new Vector2(200, 100);
+
+        Vector2 circleLocation = new Vector2(200, 200);
+        int circleRadius = 10;
+
+        Vector2 circleLastPosition;
+
+        // Rectangle sides
+        float recTopEdge = 200f;
+        //float recBottomEdge = 400f;
 
 
         /// <summary>
@@ -18,7 +29,7 @@ namespace Game10003
         /// </summary>
         public void Setup()
         {
-
+            Window.SetSize(800, 600);
         }
 
         /// <summary>
@@ -26,6 +37,27 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+            circleLastPosition = circleLocation;
+            Window.ClearBackground(Color.White);
+            Draw.FillColor = Color.Blue;
+            Draw.Rectangle(rectangleLocation, rectangleSize);
+
+            Draw.FillColor = Color.Red;
+            Draw.Circle(circleLocation, circleRadius);
+
+            
+            circleLocation.Y += 1;
+            RectangleCollision();
+        }
+
+        public void RectangleCollision()
+        {
+            bool isColliding = circleLocation.Y >= 400f;
+
+            if (isColliding == true)
+            {
+                circleLocation = circleLastPosition;
+            }
 
         }
     }
---
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.2)1/Shape_Creation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME_10003_Game_Development_Foundations___2D_Game_Template__v1._2_1
+{
+    internal class Shape_Creation
+    {
+    }
+}
