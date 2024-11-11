7d8e9aa22b9af13180bdd1709f1ccc1c3ef52f78
danco <daniel.coombs@mohawkcollege.ca>
2024-11-11 14:45:49 -0500

    ball-window collision add

--- GAME 10003 Game Development Foundations - 2D Game Template (v1.2)1/Game.cs
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.2)1/Game.cs
@@ -11,25 +11,34 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
-        Vector2 rectangleLocation = new Vector2(200, 400);
-        Vector2 rectangleSize = new Vector2(200, 100);
+        Vector2 rectangleLocation = new Vector2(50, 50);
+        Vector2 rectangleSize = new Vector2(50, 25);
+        // We need to convert our position and size into edges.
+        
 
-        Vector2 circleLocation = new Vector2(200, 200);
+        Vector2 circleLocation = new Vector2(300, 400);
         int circleRadius = 10;
+        float circleSpeed = 3;
 
         Vector2 circleLastPosition;
+        float circleDirectionY = -3;
+        float circleDirectionX = 3;
 
+        
         // Rectangle sides
         float recTopEdge = 200f;
         //float recBottomEdge = 400f;
 
-
+        float radius = 35;
+        Vector2 position = new Vector2(200, 50);
+        Vector2 velocity;
+        Vector2 forceOfGravity = new Vector2(0, 10);
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-            Window.SetSize(800, 600);
+            Window.SetSize(600, 800);
         }
 
         /// <summary>
@@ -44,21 +53,61 @@ namespace Game10003
 
             Draw.FillColor = Color.Red;
             Draw.Circle(circleLocation, circleRadius);
-
             
-            circleLocation.Y += 1;
+
+            //circleDirection = 1;
+            circleLocation.Y += circleDirectionY;
+            circleLocation.X += circleDirectionX;
+            if (DoesCollideWithBrick() == true)
+            {
+
+            }
             RectangleCollision();
         }
 
         public void RectangleCollision()
         {
-            bool isColliding = circleLocation.Y >= 400f;
-
-            if (isColliding == true)
+            //bool isColliding = circleLocation.Y >= 400f;
+            bool isBelowScreenBottom = circleLocation.Y + circleRadius >= Window.Height;
+            bool isAboveScreenTop = circleLocation.Y - circleRadius <= 0;
+            bool isPastScreenLeft = circleLocation.X - circleRadius <= 0;
+            bool isPastScreenRight = circleLocation.X + circleRadius >= Window.Width;
+            if (isBelowScreenBottom == true)
             {
-                circleLocation = circleLastPosition;
+                circleDirectionY = -3f;
+                circleLocation.Y = Window.Height - circleRadius;
             }
+            else if (isAboveScreenTop == true)
+            {
+                circleDirectionY = 3f;
+                circleLocation.Y = 0 + circleRadius;
+            }
+            else if (isPastScreenLeft == true)
+            {
+                circleDirectionX = 3f;
+                circleLocation.X = 0 + circleRadius;
+            }
+            else if (isPastScreenRight == true)
+            {
+                circleDirectionX = -3f;
+                circleLocation.X = Window.Width - circleRadius;
+            }
+        }
 
+        public bool DoesCollideWithBrick()
+        {
+            float leftEdge = rectangleLocation.X;
+            float rightEdge = rectangleLocation.X + rectangleSize.X;
+            float topEdge = rectangleLocation.Y;
+            float bottomEdge = rectangleLocation.Y + rectangleSize.Y;
+
+            bool doesOverlapLeft = leftEdge < circleLocation.X + circleRadius;
+            bool doesOverlapRight = rightEdge > circleLocation.X - circleRadius;
+            bool doesOverlapTop = topEdge < circleLocation.Y + circleRadius;
+            bool doesOverlapBottom = bottomEdge > circleLocation.Y - circleRadius;
+
+            bool doesOverlap = doesOverlapLeft && doesOverlapRight && doesOverlapTop && doesOverlapBottom;
+            return doesOverlap;
         }
     }
 }
